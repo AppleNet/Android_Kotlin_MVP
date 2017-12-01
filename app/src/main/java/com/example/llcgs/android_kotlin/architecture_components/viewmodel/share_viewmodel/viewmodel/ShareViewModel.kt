bb8c01dd6c423c54bb9ff6a6fdcb468d736edcbc
liulongchao <liulongchao@gomeholdings.com>
2017-12-01 17:56:05 +0800

    MVP_Kotlin -> Architecture-ViewModel viewModel implements SuperPresenter

--- app/src/main/java/com/example/llcgs/android_kotlin/architecture_components/viewmodel/share_viewmodel/viewmodel/ShareViewModel.kt
+++ app/src/main/java/com/example/llcgs/android_kotlin/architecture_components/viewmodel/share_viewmodel/viewmodel/ShareViewModel.kt
@@ -3,13 +3,14 @@ package com.example.llcgs.android_kotlin.architecture_components.viewmodel.share
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import com.example.llcgs.android_kotlin.architecture_components.base.presenter.BaseArchPresenter
+import com.example.llcgs.android_kotlin.base.presenter.SuperPresenter
 
 /**
  * com.example.llcgs.android_kotlin.architecture_components.viewmodel.share_viewmodel.viewmodel.ShareViewModel
  * @author liulongchao
  * @since 2017/12/1
  */
-class ShareViewModel : ViewModel(), BaseArchPresenter {
+class ShareViewModel : ViewModel(), BaseArchPresenter, SuperPresenter {
 
     private var liveData = MutableLiveData<List<String>>()
 
