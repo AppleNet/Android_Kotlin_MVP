bbd31a73b28a4457ef2c4b4eabdb21f76c463729
liulongchao <liulongchao@gomeholdings.com>
2017-12-13 17:24:34 +0800

    MVP_Kotlin -> Material Design 首页 1

---
+++ app/src/main/java/com/example/llcgs/android_kotlin/material/main/fragment/home/adapter/BroadListAdapter.kt
@@ -0,0 +1,19 @@
+package com.example.llcgs.android_kotlin.material.main.fragment.home.adapter
+
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.BaseViewHolder
+import com.example.llcgs.android_kotlin.R
+import com.example.llcgs.android_kotlin.material.main.fragment.home.bean.BroadListContent
+
+/**
+ * com.example.llcgs.android_kotlin.material.main.fragment.home.adapter.BroadListAdapter
+ * @author liulongchao
+ * @since 2017/12/13
+ */
+class BroadListAdapter: BaseQuickAdapter<BroadListContent, BaseViewHolder>(R.layout.view_home_broadcast_list_recyclerview_item) {
+
+    override fun convert(helper: BaseViewHolder?, item: BroadListContent?) {
+
+
+    }
+}
---
+++ app/src/main/java/com/example/llcgs/android_kotlin/material/main/fragment/home/bean/BroadListContent.kt
@@ -0,0 +1,10 @@
+package com.example.llcgs.android_kotlin.material.main.fragment.home.bean
+
+/**
+ * com.example.llcgs.android_kotlin.material.main.fragment.home.bean.BroadListContent
+ * @author liulongchao
+ * @since 2017/12/13
+ */
+class BroadListContent {
+
+}
