c5a1d789ed8c4081ad06521eee17ea88877f3b0f
liulongchao <liulongchao@gomeholdings.com>
2018-09-28 14:33:04 +0800

    MVP_Kotlin -> ConstraintLayout

--- app/src/main/java/com/example/llcgs/android_kotlin/constraintlayout/ConstraintLayoutActivity.kt
+++ app/src/main/java/com/example/llcgs/android_kotlin/constraintlayout/ConstraintLayoutActivity.kt
@@ -20,19 +20,51 @@ class ConstraintLayoutActivity: BaseActivity<ConstraintLayoutView, ConstraintLay
 
     // 相对位置的属性 这些属性的值即可以是parent，也可以是某个view的id。
     /***
-     *    layout_constraintLeft_toLeftOf
-          layout_constraintLeft_toRightOf
-          layout_constraintRight_toLeftOf
-          layout_constraintRight_toRightOf
-          layout_constraintTop_toTopOf
-          layout_constraintTop_toBottomOf
-          layout_constraintBottom_toTopOf
-          layout_constraintBottom_toBottomOf
-          layout_constraintBaseline_toBaselineOf
-          layout_constraintStart_toEndOf
-          layout_constraintStart_toStartOf
-          layout_constraintEnd_toStartOf
-          layout_constraintEnd_toEndOf
+     *  layout_constraintLeft_toLeftOf
+        layout_constraintLeft_toRightOf
+        layout_constraintRight_toLeftOf
+        layout_constraintRight_toRightOf
+        layout_constraintTop_toTopOf
+        layout_constraintTop_toBottomOf
+        layout_constraintBottom_toTopOf
+        layout_constraintBottom_toBottomOf
+        layout_constraintBaseline_toBaselineOf
+        layout_constraintStart_toEndOf
+        layout_constraintStart_toStartOf
+        layout_constraintEnd_toStartOf
+        layout_constraintEnd_toEndOf
+
+        # 即文章的baseline对齐
+        layout_constraintBaseline_toBaselineOf
+
+        # 与left,right类似
+        layout_constraintStart_toEndOf
+        layout_constraintStart_toStartOf
+        layout_constraintEnd_toStartOf
+        layout_constraintEnd_toEndOf
+
+        # margin不需要解释
+        android:layout_marginStart
+        android:layout_marginEnd
+        android:layout_marginLeft
+        android:layout_marginTop
+        android:layout_marginRight
+        android:layout_marginBottom
+
+        layout_constraintHorizontal_bias
+        layout_constraintVertical_bias
+
+        layout_constraintHorizontal_chainStyle
+        layout_constraintVertical_chainStyle
+
+        layout_constraintVertical_weight
+
+        Guideline
+
+        ---------------------
+
+        本文来自 鸿洋_ 的CSDN 博客 ，全文地址请点击：https://blog.csdn.net/lmj623565791/article/details/78011599?utm_source=copy
+
      * */
 
 
