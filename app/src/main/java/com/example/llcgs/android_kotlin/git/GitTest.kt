b9f92708ce1c3dfc8f4070ae7e53dd8f2aa04d54
liulongchao <657015957@qq.com>
2019-01-16 16:24:36 +0800

    Kotlin_MVP -> MVP & Git

--- app/src/main/java/com/example/llcgs/android_kotlin/git/GitTest.kt
+++ app/src/main/java/com/example/llcgs/android_kotlin/git/GitTest.kt
@@ -1,7 +1,5 @@
 package com.example.llcgs.android_kotlin.git;
 
-import com.example.llcgs.android_kotlin.utils.log.logD
-
 /**
  * com.example.llcgs.android_kotlin.git.GitTest
  * @author liulongchao
