14602feaf6fcc2a59f3a1d5a7e0f407d53e2f5d3
liulongchao <657015957@qq.com>
2019-01-19 18:41:31 +0800

    Kotlin_MVP -> add

---
+++ app/src/main/java/com/example/llcgs/android_kotlin/git/GitAddTest.kt
@@ -0,0 +1,15 @@
+package com.example.llcgs.android_kotlin.git;
+
+import com.example.llcgs.android_kotlin.utils.log.logD
+
+/**
+ * com.example.llcgs.android_kotlin.git.GitAddTest
+ * @author liulongchao
+ * @since 2019/1/19
+ */
+class GitAddTest {
+
+    fun test(){
+        "add".logD()
+    }
+}
