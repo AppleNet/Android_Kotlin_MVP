e10e5cb95a861472c4bfe75335ce1b1bb47ec5c4
liulongchao <657015957@qq.com>
2019-01-19 19:09:02 +0800

    Kotlin_MVP -> amend

---
+++ app/src/main/java/com/example/llcgs/android_kotlin/git/GitAmendTest.kt
@@ -0,0 +1,20 @@
+package com.example.llcgs.android_kotlin.git;
+
+import com.example.llcgs.android_kotlin.utils.log.logD
+
+/**
+ * com.example.llcgs.android_kotlin.git.GitAmendTest
+ * @author liulongchao
+ * @since 2019/1/19
+ */
+class GitAmendTest {
+
+    fun amend(){
+        "amend".logD()
+    }
+
+    fun amend1(){
+        "amend1".logD()
+    }
+
+}
