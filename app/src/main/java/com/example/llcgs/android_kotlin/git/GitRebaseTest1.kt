325a8ee2accf27e0083a8ae79291130a90b642a2
liulongchao <657015957@qq.com>
2019-01-19 19:03:16 +0800

    Kotlin_MVP rebase amend

---
+++ app/src/main/java/com/example/llcgs/android_kotlin/git/GitRebaseTest1.kt
@@ -0,0 +1,15 @@
+package com.example.llcgs.android_kotlin.git;
+
+import com.example.llcgs.android_kotlin.utils.log.logD
+
+/**
+ * com.example.llcgs.android_kotlin.git.GitRebaseTest1
+ * @author liulongchao
+ * @since 2019/1/19
+ */
+class GitRebaseTest1 {
+
+    fun rebase(){
+        "develop rebase".logD()
+    }
+}
