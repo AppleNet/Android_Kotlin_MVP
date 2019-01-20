6d3b92154b74caec7c985d97bb414291858e3698
liulongchao <657015957@qq.com>
2019-01-20 17:15:23 +0800

    MVP_Kotlin -> Git Window handle

--- app/src/main/java/com/example/llcgs/android_kotlin/git/GitCleanTest.kt
+++ app/src/main/java/com/example/llcgs/android_kotlin/git/GitCleanTest.kt
@@ -1,9 +1,19 @@
 package com.example.llcgs.android_kotlin.git;
 
+import com.example.llcgs.android_kotlin.utils.log.logD
+
 /**
  * com.example.llcgs.android_kotlin.git.GitCleanTest
  * @author liulongchao
  * @since 2019/1/20
  */
 class GitCleanTest {
+
+    fun windowAmend(){
+        "windowAmend".logD()
+    }
+
+    fun windowAmend1(){
+        "windowAmend1".logD()
+    }
 }
