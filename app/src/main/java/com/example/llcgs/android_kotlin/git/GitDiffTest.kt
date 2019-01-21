ff9ffd1d25f03d1660dd628be4c62e3b3766557b
liulongchao <657015957@qq.com>
2019-01-21 11:29:35 +0800

    Kotlin_MVP -> diff

---
+++ app/src/main/java/com/example/llcgs/android_kotlin/git/GitDiffTest.kt
@@ -0,0 +1,15 @@
+package com.example.llcgs.android_kotlin.git;
+
+import com.example.llcgs.android_kotlin.utils.log.logD
+
+/**
+ * com.example.llcgs.android_kotlin.git.GitDiffTest
+ * @author liulongchao
+ * @since 2019/1/21
+ */
+class GitDiffTest {
+
+    fun diff(){
+        "diff".logD()
+    }
+}
