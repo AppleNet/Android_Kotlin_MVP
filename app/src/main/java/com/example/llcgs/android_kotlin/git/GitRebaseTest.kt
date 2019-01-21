55fd8afa13d9b7e0eecf24940d6bbdb002518cc5
liulongchao <657015957@qq.com>
2019-01-21 17:13:54 +0800

    MVP_Kotlin -> rebase -i -

---
+++ app/src/main/java/com/example/llcgs/android_kotlin/git/GitRebaseTest.kt
@@ -0,0 +1,9 @@
+package com.example.llcgs.android_kotlin.git;
+
+/**
+ * com.example.llcgs.android_kotlin.git.GitRebaseTest
+ * @author liulongchao
+ * @since 2019/1/21
+ */
+class GitRebaseTest {
+}
