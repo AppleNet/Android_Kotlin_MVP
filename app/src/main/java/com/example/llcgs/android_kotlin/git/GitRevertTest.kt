d87e03ca78b14ff8c0eb853a4b7e33182619764a
liulongchao <657015957@qq.com>
2019-01-22 14:45:12 +0800

    Revert "MVP_Kotlin -> revert"
    This reverts commit 70a974d0345261328dea93756c53a0fc1c183564.

--- app/src/main/java/com/example/llcgs/android_kotlin/git/GitRevertTest.kt
+++
@@ -1,9 +0,0 @@
-package com.example.llcgs.android_kotlin.git;
-
-/**
- * com.example.llcgs.android_kotlin.git.GitRevertTest
- * @author liulongchao
- * @since 2019/1/22
- */
-class GitRevertTest {
-}
